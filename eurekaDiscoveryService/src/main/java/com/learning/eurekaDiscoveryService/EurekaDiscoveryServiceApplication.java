5b04d621f099069327c426ee35e75fd2c6740491
Niks1820 <38504842+Niks1820@users.noreply.github.com>
2018-11-12 13:57:28 -0800

    Springboot Microservice with Eureka Dsicovery
    This project configures the Springboot Microservice with Neflix Eureka Dsicovery service. To enable the client to be discovered the client discovery and eureka url need to be added.

---
+++ eurekaDiscoveryService/src/main/java/com/learning/eurekaDiscoveryService/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.learning.eurekaDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+	}
+}
